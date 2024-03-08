b40497bcc0131c1e8582122a08f39402d936354b
zalaya <enol.corzal@gmail.com>
2024-03-08 21:52:57 +0200

    chore: create the project structure

---
+++ src/main/java/org/backend/issuementor/IssuementorApplication.java
@@ -0,0 +1,13 @@
+package org.backend.issuementor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuementorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssuementorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/backend/issuementor/IssuementorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.issuementor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuementorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
