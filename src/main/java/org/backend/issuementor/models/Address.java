6b975165fc0161419fd56c308ef1039d365d9a22
zalaya <enol.corzal@gmail.com>
2024-03-09 13:40:30 +0100

    feat: add the address model and update the script.sql

---
+++ src/main/java/org/backend/issuementor/models/Address.java
@@ -0,0 +1,27 @@
+package org.backend.issuementor.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "province")
+    private String province;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Column(name = "country")
+    private String country;
+}
