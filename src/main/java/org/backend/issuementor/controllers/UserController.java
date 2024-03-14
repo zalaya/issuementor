193207f7dac8c9516bad7c8bab983b6999d7ea34
zalaya <enol.corzal@gmail.com>
2024-03-14 23:28:00 +0100

    refactor: remove abstract controller class

--- src/main/java/org/backend/issuementor/controllers/UserController.java
+++ src/main/java/org/backend/issuementor/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.backend.issuementor.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,12 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
-public class UserController extends AbstractController {
+public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private JWTService jwtService;
@@ -28,23 +32,18 @@ public class UserController extends AbstractController {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
 
-        user.setPassword(encode(user.getPassword()));
-
-        return new ResponseEntity<>(
-            user, HttpStatus.OK
-        );
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody User user) {
         Optional<User> databaseUser = userService.findByEmail(user.getEmail());
 
-        if (databaseUser.isEmpty() || match(user, databaseUser.get())) {
+        if (databaseUser.isEmpty() || passwordEncoder.matches(user.getPassword(), databaseUser.get().getPassword())) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
-        return new ResponseEntity<>(
-            jwtService.generate(databaseUser.get().getId()), HttpStatus.OK
-        );
+        return new ResponseEntity<>(jwtService.generate(databaseUser.get().getId()), HttpStatus.OK);
     }
 }
