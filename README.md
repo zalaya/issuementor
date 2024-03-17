8102f15da5e6bba711309faeac2e25e24fc8d93c
zalaya <enol.corzal@gmail.com>
2024-03-17 03:30:12 +0100

    feat: add a todo list in the readme file and change the script.sql

---
+++ README.md
@@ -0,0 +1,21 @@
+## TODO
+
+### Sign Up.
+
+- [ ] Validate email.
+    - [ ] Send email.
+    - [ ] Confirm email.
+- [ ] Validate password.
+    - [ ] Set minimum length.
+    - [ ] Create security meter.
+- [ ] Pre-filled username by email.
+- [ ] Encode password.
+- [ ] Save user.
+- [ ] Return basic information.
+
+### Log in.
+
+- [ ] Check email.
+- [ ] Check password.
+- [ ] Block account if logged in incorrectly.
+- [ ] Return a token.
--- src/main/resources/database/script.sql
+++ src/main/resources/database/script.sql
@@ -4,75 +4,77 @@ USE issuementor;
 
 CREATE TABLE addresses (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    street VARCHAR(150) NOT NULL,
-    city VARCHAR(100) NOT NULL,
-    country VARCHAR(100) NOT NULL,
-    postal_code VARCHAR(20) NOT NULL
+    street VARCHAR(150),
+    city VARCHAR(100),
+    country VARCHAR(100),
+    postal_code VARCHAR(20)
 );
 
 CREATE TABLE companies (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    address_id BIGINT NOT NULL,
+    name VARCHAR(100),
+    address_id BIGINT,
     FOREIGN KEY (address_id) REFERENCES addresses(id)
 );
 
 CREATE TABLE users (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    surname VARCHAR(100) NOT NULL,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    phone VARCHAR(20) UNIQUE NOT NULL,
-    birth_date DATE NOT NULL,
-    creation_date TIMESTAMP NOT NULL,
+    name VARCHAR(50),
+    surname VARCHAR(100),
+    username VARCHAR(50),
+    email VARCHAR(100),
+    password VARCHAR(100),
+    phone VARCHAR(20),
+    birth_date DATE,
+    creation_date TIMESTAMP,
     login_date TIMESTAMP,
-    role ENUM('USER', 'ADMIN', 'TECHNICIAN') NOT NULL,
-    gender ENUM('MALE', 'FEMALE', 'OTHER') NOT NULL,
+    role ENUM('USER', 'ADMIN', 'TECHNICIAN'),
+    gender ENUM('MALE', 'FEMALE', 'OTHER'),
     address_id BIGINT,
     company_id BIGINT,
     FOREIGN KEY (address_id) REFERENCES addresses(id),
     FOREIGN KEY (company_id) REFERENCES companies(id)
 );
 
+CREATE TABLE products (
+                          id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                          name VARCHAR(100),
+                          description TEXT,
+                          user_id BIGINT,
+                          company_id BIGINT,
+                          FOREIGN KEY (user_id) REFERENCES users(id),
+                          FOREIGN KEY (company_id) REFERENCES companies(id)
+);
+
 CREATE TABLE incidences (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    description TEXT NOT NULL,
-    creation_date TIMESTAMP NOT NULL,
-    priority ENUM('LOW', 'MEDIUM', 'HIGH') NOT NULL,
-    status ENUM('OPEN', 'PENDING', 'RESOLVED', 'CLOSED') NOT NULL,
-    user_id BIGINT NOT NULL,
+    name VARCHAR(100),
+    description TEXT,
+    creation_date TIMESTAMP,
+    priority ENUM('LOW', 'MEDIUM', 'HIGH'),
+    status ENUM('OPEN', 'PENDING', 'RESOLVED', 'CLOSED'),
+    product_id BIGINT,
+    user_id BIGINT,
     technician_id BIGINT,
+    FOREIGN KEY (product_id) REFERENCES products(id),
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (technician_id) REFERENCES users(id)
 );
 
 CREATE TABLE attachments (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    url VARCHAR(255) NOT NULL,
-    creation_date TIMESTAMP NOT NULL,
-    incidence_id BIGINT NOT NULL,
-    user_id BIGINT NOT NULL,
+    name VARCHAR(255),
+    url VARCHAR(255),
+    creation_date TIMESTAMP,
+    incidence_id BIGINT,
+    user_id BIGINT,
     FOREIGN KEY (incidence_id) REFERENCES incidences(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE products (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    description TEXT,
-    user_id BIGINT NOT NULL,
-    company_id BIGINT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (company_id) REFERENCES companies(id)
-);
-
 CREATE TABLE categories (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) UNIQUE NOT NULL
+    name VARCHAR(50)
 );
 
 CREATE TABLE product_categories (
@@ -85,10 +87,10 @@ CREATE TABLE product_categories (
 
 CREATE TABLE comments (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    content TEXT NOT NULL,
-    creation_date TIMESTAMP NOT NULL,
-    user_id BIGINT NOT NULL,
-    incidence_id BIGINT NOT NULL,
+    content TEXT,
+    creation_date TIMESTAMP,
+    user_id BIGINT,
+    incidence_id BIGINT,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (incidence_id) REFERENCES incidences(id)
 );
