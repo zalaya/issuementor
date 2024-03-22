770076055e42d52bb123e1718fa659ade63dcdda
zalaya <enol.corzal@gmail.com>
2024-03-22 19:36:10 +0100

    feat: add the incidence repository and service

---
+++ src/main/java/org/backend/issuementor/services/IncidenceService.java
@@ -0,0 +1,5 @@
+package org.backend.issuementor.services;
+
+public interface IncidenceService {
+
+}
---
+++ src/main/java/org/backend/issuementor/services/implementations/IncidenceServiceImplementation.java
@@ -0,0 +1,7 @@
+package org.backend.issuementor.services.implementations;
+
+import org.backend.issuementor.services.IncidenceService;
+
+public class IncidenceServiceImplementation implements IncidenceService {
+
+}
